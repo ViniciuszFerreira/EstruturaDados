363025592e402f6686f6337b1fa0f2ac58897e72
ViniciuszFerreira <viniefs2012@gmail.com>
2025-03-28 13:09:11 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,25 +3,48 @@
 using namespace std;
 
 int main() {
-    int idade;
+    int idade, op;
+    int totalPessoas = 0, somaIdades = 0;
+    int contOp1 = 0, contOp2 = 0, contOp3 = 0, contOp4 = 0;
 
-    // Pergunte a idade do nadador
-    cout << "Digite a idade do nadador: ";
-    cin >> idade;
+    cout << "Digite a idade e a opinião (1-Ótimo, 2-Bom, 3-Regular, 4-Ruim). Digite idade negativa para sair.\n";
 
-    // Verificar a categoria
-    if (idade >= 5 && idade <= 7) {
-        cout << "Categoria: Infantil A" << endl;
-    } else if (idade >= 8 && idade <= 10) {
-        cout << "Categoria: Infantil B" << endl;
-    } else if (idade >= 11 && idade <= 13) {
-        cout << "Categoria: Juvenil A" << endl;
-    } else if (idade >= 14 && idade <= 17) {
-        cout << "Categoria: Juvenil B" << endl;
-    } else if (idade > 17) {
-        cout << "Categoria: Sênior" << endl;
+    while (true) {
+        cout << "Idade: ";
+        cin >> idade;
+
+        if (idade < 0) break; // Encerra a entrada de dados
+
+        cout << "Opinião: ";
+        cin >> op;
+
+        if (op < 1 || op > 4) {
+            cout << "Opinião inválida! Digite um valor entre 1 e 4.\n";
+            continue;
+        }
+
+        totalPessoas++;
+        somaIdades += idade;
+
+        switch (op) {
+            case 1: contOp1++; break;
+            case 2: contOp2++; break;
+            case 3: contOp3++; break;
+            case 4: contOp4++; break;
+        }
+    }
+
+    if (totalPessoas > 0) {
+        cout << "\nResultados da pesquisa:\n";
+        cout << "A. Total de pessoas que responderam: " << totalPessoas << endl;
+        cout << "B. Média de idade: " << (double)somaIdades / totalPessoas << endl;
+        cout << "C. Porcentagem de cada resposta:\n";
+        cout << "   Ótimo: " << (contOp1 * 100.0 / totalPessoas) << "%\n";
+        cout << "   Bom: " << (contOp2 * 100.0 / totalPessoas) << "%\n";
+        cout << "   Regular: " << (contOp3 * 100.0 / totalPessoas) << "%\n";
+        cout << "   Ruim: " << (contOp4 * 100.0 / totalPessoas) << "%\n";
     } else {
-        cout << "Idade de fora das categorias de natação!" << endl;
+        cout << "Nenhuma resposta foi registrada.\n";
     }
 
     return 0;
