3793f48229593a856514eaafb9aec4a755cb9397
ViniciuszFerreira <viniefs2012@gmail.com>
2025-03-28 13:14:23 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,50 +2,23 @@
 
 using namespace std;
 
-int main() {
-    int idade, op;
-    int totalPessoas = 0, somaIdades = 0;
-    int contOp1 = 0, contOp2 = 0, contOp3 = 0, contOp4 = 0;
+int main()
+{
+    float numHoras, salMin, salReceber;
+    float horaTrab, salBruto, imposto;
 
-    cout << "Digite a idade e a opinião (1-Ótimo, 2-Bom, 3-Regular, 4-Ruim). Digite idade negativa para sair.\n";
+    cout << "Digite o número de horas trabalhadas: " << endl;
+    cin >> numHoras;
+    cout << "Digite o valor do salário mínimo: " << endl;
+    cin >> salMin;
 
-    while (true) {
-        cout << "Idade: ";
-        cin >> idade;
+    horaTrab = salMin / 2;
+    salBruto = numHoras * horaTrab;
+    imposto = salBruto * 3/100;
+    salReceber = salBruto - imposto;
 
-        if (idade < 0) break; // Encerra a entrada de dados
-
-        cout << "Opinião: ";
-        cin >> op;
-
-        if (op < 1 || op > 4) {
-            cout << "Opinião inválida! Digite um valor entre 1 e 4.\n";
-            continue;
-        }
-
-        totalPessoas++;
-        somaIdades += idade;
-
-        switch (op) {
-            case 1: contOp1++; break;
-            case 2: contOp2++; break;
-            case 3: contOp3++; break;
-            case 4: contOp4++; break;
-        }
-    }
-
-    if (totalPessoas > 0) {
-        cout << "\nResultados da pesquisa:\n";
-        cout << "A. Total de pessoas que responderam: " << totalPessoas << endl;
-        cout << "B. Média de idade: " << (double)somaIdades / totalPessoas << endl;
-        cout << "C. Porcentagem de cada resposta:\n";
-        cout << "   Ótimo: " << (contOp1 * 100.0 / totalPessoas) << "%\n";
-        cout << "   Bom: " << (contOp2 * 100.0 / totalPessoas) << "%\n";
-        cout << "   Regular: " << (contOp3 * 100.0 / totalPessoas) << "%\n";
-        cout << "   Ruim: " << (contOp4 * 100.0 / totalPessoas) << "%\n";
-    } else {
-        cout << "Nenhuma resposta foi registrada.\n";
-    }
+    cout << "Salário Bruto: " << salBruto << endl;
+    cout << "Salário a receber: " << salReceber << endl;
 
     return 0;
 }
